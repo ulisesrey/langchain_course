61d8028cf4f07b985f0e0d3ec0788307dd97605b
ulisesrey <ulises.rey.torne@gmail.com>
2025-06-12 15:17:10 +0200

    ice breaker example

---
+++ ice_breaker.py
@@ -0,0 +1,11 @@
+import os
+# from dotenv import load_dotenv
+
+# # Load environment variables from .env file
+# load_dotenv()
+
+if __name__ == "__main__":
+    print("Hello LangChain.")
+    API_KEY = os.environ["OPENAI_API_KEY"]
+    print(API_KEY)
+    
