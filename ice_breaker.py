e7921750190aae4939170007cb817ef1880c8666
ulisesrey <ulises.rey.torne@gmail.com>
2025-06-12 15:25:48 +0200

    update ice breaker

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,8 +1,8 @@
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# # Load environment variables from .env file
-# load_dotenv()
+# Load environment variables from .env file
+load_dotenv()
 
 if __name__ == "__main__":
     print("Hello LangChain.")
