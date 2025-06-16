faa566aff5fa5f1eb35ee5bb21aecdd6f2743bba
ulisesrey <ulises.rey.torne@gmail.com>
2025-06-16 10:44:07 +0200

    add useful comment

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
+# SOME CODE IS ACTUALLY ON PROMOT.PY
+
 if __name__ == "__main__":
     print("Hello LangChain.")
     API_KEY = os.environ["OPENAI_API_KEY"]
